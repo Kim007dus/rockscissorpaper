231b6572a4f33df18108f353b4fbd3a8334c1941
Kim007dus <122279504+Kim007dus@users.noreply.github.com>
2023-02-08 20:19:10 +0100

    add first function that is not working yet

---
+++ javascript.js
@@ -0,0 +1,29 @@
+let RandomNumber = Math.floor (Math.random()*3)
+
+function getComputerChoice () {
+    return RandomNumber
+    switch (RandomNumber().getChoice()) { 
+        case 0: 
+        choice = "Rock";
+        break;
+        case 1: 
+        choice = "Paper";
+        break;
+        case 2: 
+        choice = "Scissor";
+         }
+        }
+
+ 
+ 
+    
+
+
+
+
+
+
+
+    
+
+
