fc0a72aa71a17fdb579d232e5fa735091f89de47
Kim007dus <122279504+Kim007dus@users.noreply.github.com>
2023-02-09 13:06:09 +0100

    add function getComputerChoice working

--- javascript.js
+++ javascript.js
@@ -1,17 +1,16 @@
 
-let RandomNumber = Math.floor (Math.random()*3) 
-switch (RandomNumber) { 
-        case 0: 
-        choice = "Rock"
-        break;
-        case 1: 
-        choice = "Paper"
-        break;
-        case 2: 
-        choice = "Scissor"
-        }
-
   
 function getComputerChoice () {
-    return RandomNumber;
+    let RandomNumber = Math.floor (Math.random()*3) 
+    switch (RandomNumber) { 
+            case 0: 
+            return "Rock"
+            break;
+            case 1: 
+            return "Paper"
+            break;
+            case 2: 
+            return "Scissor"
+            break;
+            };
 }
