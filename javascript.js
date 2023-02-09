7601338655d597ecb5fdd19e5c2d5913e16ed43a
Kim007dus <122279504+Kim007dus@users.noreply.github.com>
2023-02-09 16:47:54 +0100

    add if statements and console.log working

--- javascript.js
+++ javascript.js
@@ -15,9 +15,42 @@ function getComputerChoice () {
             };
 }
 
+let playerSelection = prompt ("Let's play!! Choose between rock, scissor or paper:")
+console.log ("Your choice is " + playerSelection)
+
 const computerSelection = getComputerChoice ()
-let playerSelection = prompt ("Do you want Rock, Scissor or Paper?")
+console.log ("Computer got " + computerSelection)
+
+function playRound (computerSelection, playerSelection)  {  
+    if (computerSelection === "Paper" && playerSelection === "Rock") {
+    return "You lose, paper beats rock"
+    }
+    
+    else if (computerSelection === "Paper" && playerSelection === "Scissor"){
+    return "You win, scissor beats paper"
+    }
+   
+    if (computerSelection === "Rock" && playerSelection === "Paper") {
+        return "You win, paper beats rock"
+    }
+    
+    else if (computerSelection === "Rock" && playerSelection === "Scissor"){
+        return "You lose, rock beats scissor"
+    }
+
+    if (computerSelection === "Scissor" && playerSelection === "Paper") {
+        return "You lose, scissor beats paper"
+    }
+
+    else if (computerSelection === "Scissor" && playerSelection === "Rock") {
+        return "You win, rock beats scissor"
+    }
+
+    else (computerSelection, playerSelection) 
+        return "It's a tie"
+    
+  
+ }
 
 
-function playRound (computerSelection, playerSelection) {
-}
+console.log (playRound(computerSelection, playerSelection))
