86a3459add3ae6d38dd00c9badf0438c81c85689
Kim007dus <122279504+Kim007dus@users.noreply.github.com>
2023-02-11 17:41:08 +0100

    add loop for 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
+let playerScore = 0
+let computerScore = 0
 
-  
 function getComputerChoice () {
     let RandomNumber = Math.floor (Math.random()*3) 
     switch (RandomNumber) { 
@@ -15,43 +16,79 @@ function getComputerChoice () {
             };
 }
 
-let playerSelection = prompt ("Let's play!! Choose between rock, scissor or paper:").toLowerCase()
+function getPlayerSelection () { 
+    return prompt ("Let's play!! Choose between rock, scissor or paper:").toLowerCase()
+}
+
+const playerSelection = getPlayerSelection ()
+const computerSelection = getComputerChoice ()
 
 console.log ("Your choice is " + playerSelection)
-
-const computerSelection = getComputerChoice ()
 console.log ("Computer got " + computerSelection)
 
 function playRound (computerSelection, playerSelection)  {  
     if (computerSelection === "paper" && playerSelection === "rock") {
-    return "You lose, paper beats rock"
+        computerScore += 1
+        return "You lose, paper beats rock"
+        
     }
     
     else if (computerSelection === "paper" && playerSelection === "scissor"){
-    return "You win, scissor beats paper"
+        playerScore += 1
+        return "You win, scissor beats paper"
+        
     }
    
     if (computerSelection === "rock" && playerSelection === "paper") {
+        playerScore += 1
         return "You win, paper beats rock"
+        
     }
     
     else if (computerSelection === "rock" && playerSelection === "scissor"){
+        computerScore += 1
         return "You lose, rock beats scissor"
+        
     }
 
     if (computerSelection === "scissor" && playerSelection === "paper") {
+        computerScore += 1
         return "You lose, scissor beats paper"
+        
     }
 
     else if (computerSelection === "scissor" && playerSelection === "rock") {
+        playerScore += 1
         return "You win, rock beats scissor"
+        
     }
 
     else (computerSelection, playerSelection) 
-        return "It's a tie"
-    
+        computerScore += 1
+        playerScore += 1    
+        return "Oh no, draw! No points"
+
   
  }
 
 
-console.log (playRound(computerSelection, playerSelection))
+
+const playRoundResult = playRound(computerSelection, playerSelection)
+console.log ("! " + playRoundResult)
+
+for (let i=0; i < 5; i++) {
+    
+    const playerSelection = getPlayerSelection ()
+    const computerSelection = getComputerChoice ()
+    const playRoundResult = playRound(computerSelection, playerSelection)
+
+    console.log ("Your choice is " + playerSelection)
+    console.log ("Computer got " + computerSelection)
+    console.log ("! " + playRoundResult)
+}
+
+console.log ("Computer: " + computerScore)
+console.log ("You: " + playerScore)
+
+ 
+
